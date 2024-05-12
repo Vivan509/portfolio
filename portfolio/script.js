3fb6525c62155400d469f140e40d87857626052e
Gulshan Kumar <159818951+Vivan509@users.noreply.github.com>
2024-05-12 17:47:49 +0000

    Pending changes exported from your codespace

---
+++ portfolio/script.js
@@ -0,0 +1,46 @@
+
+
+gsap.from('.logo h3',{
+  y: '-20',
+  opacity: '0',
+  duration: '2',
+  delay:'1',
+
+})
+gsap.from('.navbar-links li',{
+  y: '-20',
+  opacity: '0',
+  duration: '2',
+  delay:'1',
+ stagger: '.3'
+})
+
+
+gsap.from('.subtitle h1',{
+  x:'-50',
+  opacity: '0',
+  duration: '2',
+  delay: '1',
+})
+
+gsap.from('.title',{
+  x:'-50',
+  opacity: '0',
+  duration: '2',
+  delay: '1',
+})
+
+gsap.from('.title-desc',{
+  x:'-50',
+  opacity: '0',
+  duration: '2',
+  delay: '1', 
+})
+
+
+
+
+
+
+
+
